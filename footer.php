9d0f53b12d0b3680b07437c55d29b2cdc9cef608
J0WI <J0WI@users.noreply.github.com>
2015-04-26 18:01:06 +0200

    initial commit

---
+++ footer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+	</div><!-- #main .wrapper -->
+	<footer id="colophon" role="contentinfo">
+		<div class="site-info">
+			<!-- =Individual by mach-politik.ch -->
+			<a href="https://creativecommons.org/licenses/by-sa/4.0/deed.de" rel="license" target="_blank"><img src="/wp-content/uploads/2014/03/Creative_Commons_20t.png" alt="Creative Commons" /></a>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
