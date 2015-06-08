b94cd1fb5804d122db1e57c4c28b352893da6c96
J0WI <J0WI@users.noreply.github.com>
2015-06-08 16:28:39 +0200

    code style

--- header.php
+++ header.php
@@ -53,24 +53,29 @@
 </head>
 
 	<body <?php body_class(); ?>>
-<nav id="global-bar"><div class="left"><a href="https://mach-politik.ch">Hauptseite</a><a href="https://mach-politik.ch/projekte/projects/projekte">Projekte</a></div><div class="right"><a href="https://mach-politik.ch/projekte/login">Login</a><a href="https://mach-politik.ch/projekte/account/register">Registrieren</a></div></nav>
-<div id="page" class="hfeed site">
-	<header id="masthead" class="site-header" role="banner">
-		<hgroup>
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</hgroup>
-
-		<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-			<img alt="Logo Mach-Politik.ch" width="960" height="250" src="../images/logo-B-2farbig-halbkreis-header-web.svg"
-		</a>
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<h3 class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></h3>
-			<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-
-	</header><!-- #masthead -->
-
-	<div id="main" class="wrapper">
+		<nav id="global-bar">
+			<div class="left">
+				<a href="https://mach-politik.ch">Hauptseite</a>
+				<a href="https://mach-politik.ch/projekte/projects/projekte">Projekte</a>
+			</div>
+			<div class="right">
+				<a href="https://mach-politik.ch/projekte/login">Login</a>
+				<a href="https://mach-politik.ch/projekte/account/register">Registrieren</a>
+			</div>
+		</nav>
+		<div id="page" class="hfeed site">
+			<header id="masthead" class="site-header" role="banner">
+				<hgroup>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+				</hgroup>
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+					<img alt="Logo Mach-Politik.ch" width="960" height="250" src="../images/logo-B-2farbig-halbkreis-header-web.svg"
+				</a>
+				<nav id="site-navigation" class="main-navigation" role="navigation">
+					<h3 class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></h3>
+					<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+				</nav><!-- #site-navigation -->
+			</header><!-- #masthead -->
+			<div id="main" class="wrapper">
