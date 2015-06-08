81a33baf7eeccf20b7c94882b565682d0a11a757
J0WI <J0WI@users.noreply.github.com>
2015-06-08 16:59:59 +0200

    code style

--- header.php
+++ header.php
@@ -19,37 +19,37 @@
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="apple-touch-icon" sizes="57x57" href="https://mach-politik.ch/favicon/apple-touch-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="114x114" href="https://mach-politik.ch/favicon/apple-touch-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="72x72" href="https://mach-politik.ch/favicon/apple-touch-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="144x144" href="https://mach-politik.ch/favicon/apple-touch-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="60x60" href="https://mach-politik.ch/favicon/apple-touch-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="120x120" href="https://mach-politik.ch/favicon/apple-touch-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="76x76" href="https://mach-politik.ch/favicon/apple-touch-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="152x152" href="https://mach-politik.ch/favicon/apple-touch-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="https://mach-politik.ch/favicon/apple-touch-icon-180x180.png">
-<meta name="apple-mobile-web-app-title" content="mach-politik.ch">
-<link rel="shortcut icon" href="https://mach-politik.ch/favicon/favicon.ico" />
-<link rel="icon" type="image/png" href="https://mach-politik.ch/favicon/favicon-192x192.png" sizes="192x192">
-<link rel="icon" type="image/png" href="https://mach-politik.ch/favicon/favicon-160x160.png" sizes="160x160">
-<link rel="icon" type="image/png" href="https://mach-politik.ch/favicon/favicon-96x96.png" sizes="96x96">
-<link rel="icon" type="image/png" href="https://mach-politik.ch/favicon/favicon-16x16.png" sizes="16x16">
-<link rel="icon" type="image/png" href="https://mach-politik.ch/favicon/favicon-32x32.png" sizes="32x32">
-<meta name="msapplication-TileColor" content="#87ceeb">
-<meta name="msapplication-TileImage" content="https://mach-politik.ch/favicon/mstile-144x144.png">
-<meta name="application-name" content="mach-politik.ch">
-<meta name="msapplication-TileColor" content="#87ceeb">
-<meta name="msapplication-TileImage" content="https://mach-politik.ch/favicon/mstile-144x144.png">
-<meta name="application-name" content="mach-politik.ch">
-<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
-<!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<![endif]-->
-<?php wp_head(); ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width" />
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="apple-touch-icon" sizes="57x57" href="https://mach-politik.ch/favicon/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="https://mach-politik.ch/favicon/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="https://mach-politik.ch/favicon/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="https://mach-politik.ch/favicon/apple-touch-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="https://mach-politik.ch/favicon/apple-touch-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="https://mach-politik.ch/favicon/apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="https://mach-politik.ch/favicon/apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="https://mach-politik.ch/favicon/apple-touch-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="https://mach-politik.ch/favicon/apple-touch-icon-180x180.png">
+	<meta name="apple-mobile-web-app-title" content="mach-politik.ch">
+	<link rel="shortcut icon" href="https://mach-politik.ch/favicon/favicon.ico" />
+	<link rel="icon" type="image/png" href="https://mach-politik.ch/favicon/favicon-192x192.png" sizes="192x192">
+	<link rel="icon" type="image/png" href="https://mach-politik.ch/favicon/favicon-160x160.png" sizes="160x160">
+	<link rel="icon" type="image/png" href="https://mach-politik.ch/favicon/favicon-96x96.png" sizes="96x96">
+	<link rel="icon" type="image/png" href="https://mach-politik.ch/favicon/favicon-16x16.png" sizes="16x16">
+	<link rel="icon" type="image/png" href="https://mach-politik.ch/favicon/favicon-32x32.png" sizes="32x32">
+	<meta name="msapplication-TileColor" content="#87ceeb">
+	<meta name="msapplication-TileImage" content="https://mach-politik.ch/favicon/mstile-144x144.png">
+	<meta name="application-name" content="mach-politik.ch">
+	<meta name="msapplication-TileColor" content="#87ceeb">
+	<meta name="msapplication-TileImage" content="https://mach-politik.ch/favicon/mstile-144x144.png">
+	<meta name="application-name" content="mach-politik.ch">
+	<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
+	<!--[if lt IE 9]>
+		<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+	<![endif]-->
+	<?php wp_head(); ?>
 </head>
 
 	<body <?php body_class(); ?>>
@@ -70,7 +70,7 @@
 					<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 				</hgroup>
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-				<img alt="Logo Mach-Politik.ch" width="960" height="250" src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo-B-2farbig-halbkreis-header-web.svg"
+				<img alt="Logo Mach-Politik.ch" width="960" height="250" src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo-B-2farbig-halbkreis-header-web.svg">
 				</a>
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					<h3 class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></h3>
