eba242e66b6e1c311fa1d819ebbde85944bf81fb
J0WI <J0WI@users.noreply.github.com>
2015-12-07 23:04:13 +0100

    fixed tile color

--- header.php
+++ header.php
@@ -39,10 +39,10 @@
 	<link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96">
 	<link rel="icon" type="image/png" href="/favicon/favicon-16x16.png" sizes="16x16">
 	<link rel="icon" type="image/png" href="/favicon/favicon-32x32.png" sizes="32x32">
-	<meta name="msapplication-TileColor" content="#87ceeb">
+	<meta name="msapplication-TileColor" content="#6EC4E9">
 	<meta name="msapplication-TileImage" content="/favicon/mstile-144x144.png">
 	<meta name="application-name" content="mach-politik.ch">
-	<meta name="msapplication-TileColor" content="#87ceeb">
+	<meta name="msapplication-TileColor" content="#6EC4E9">
 	<meta name="msapplication-TileImage" content="/favicon/mstile-144x144.png">
 	<meta name="application-name" content="mach-politik.ch">
 	<?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
